25e218212afb33fa6643a870deedfb6fd40fea52
KS Lee <KS Lee>
2017-02-27 20:27:48 +0800

    Updated genre list

--- messages/index.js
+++ messages/index.js
@@ -39,20 +39,50 @@ const genres = {
   comedy: {
     id: 35,
   },
+  crime:{
+    id:35,
+  },
   documentary: {
     id: 99,
   },
   drama: {
     id: 18,
   },
+  family:{
+    id:10751,
+  },
+  fantasy:{
+    id:14,
+  },
+  history:{
+    id:36,
+  },
   horror: {
     id: 27,
+  },
+  music:{
+    id:10402,
   },
   mystery: {
     id: 9648,
   },
   romance: {
     id: 10749,
+  },
+  science fiction:{
+    id:878,
+  },
+  tv movie:{
+    id:10770,
+  },
+  thriller:{
+    id:53,
+  },
+  war:{
+    id:10752,
+  },
+  western:{
+    id:37,
   },
   '(quit)': {
     id: 0,
